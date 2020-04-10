dcb14f27f9e33a1ee82791f0a2a762e5a7e6251f
Jesus Garcia <jelegaho@gmail.com>
2020-04-09 20:17:06 -0500

    entendiendo async y await

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+const XMLHTTPRequest = require('xmlhttprequest').XMLHttpRequest
+
+const fecthData = (url_api) => {
+
+    return new Promise((resolve, reject) => {
+
+        const xhttp = new XMLHTTPRequest()
+
+        xhttp.open('GET', url_api, true)
+        xhttp.onreadystatechange = (() => {
+            if(xhttp.readyState === 4){
+
+                (xhttp.status === 200)
+                    ? resolve(JSON.parse(xhttp.responseText)) 
+                    : reject(new Error(`Error ${url_api}`))
+            }
+        })
+        xhttp.send()
+    })
+}
+
+module.exports = fecthData;
